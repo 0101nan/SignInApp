0813788376435c94891373ee4d803866505d01b9
0101nan <liyinan.heaven@hotmail.com>
2019-07-14 23:05:02 +0800

    Initial commit

--- app/src/main/java/com/liyinan/signin/activity/ChoosePersonActivity.java
+++ app/src/main/java/com/liyinan/signin/activity/ChoosePersonActivity.java
@@ -1,15 +1,239 @@
 package com.liyinan.signin.activity;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.liyinan.signin.R;
+import com.liyinan.signin.db.Departmant;
+import com.liyinan.signin.db.Person;
+import com.liyinan.signin.util.Utility;
+
+import org.litepal.LitePal;
+
+import java.util.ArrayList;
+import java.util.List;
+
 
 public class ChoosePersonActivity extends AppCompatActivity {
-
+    public static final int LEVEL_DEPARTMENT=0;
+    public static final int LEVEL_PERSON=1;
+    public static final int ALL_PERSON=2;
+    public static final int WAIT_PERSON=3;
+    public static final int LEAVE_PERSON=4;
+    private List<Departmant> departmantList=new ArrayList<>();
+    private List<Person> personList=new ArrayList<>();
+    private ListView listView;
+    private ArrayAdapter<String> adapter;
+    private List<String> dataList=new ArrayList<>();
+    private int currentLevel;
+    private Departmant selectedDepartment;
+    private Person selectedPerson;
+    private int extra_nav;
+    private Toolbar toolbar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_choose_person);
+        Intent intent=getIntent();
+        extra_nav=intent.getIntExtra("extra_nav",0);
+        listView=findViewById(R.id.list_view);
+        toolbar=findViewById(R.id.person_toolbar);
+        adapter=new ArrayAdapter<>(this,android.R.layout.simple_list_item_1,dataList);
+        listView.setAdapter(adapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                if(currentLevel==LEVEL_DEPARTMENT){
+                    selectedDepartment=departmantList.get(position);
+                    queryPerson();
+                }else if (currentLevel==LEVEL_PERSON){
+                    selectedPerson=personList.get(position);
+                    AlertDialog.Builder dialog=new AlertDialog.Builder(ChoosePersonActivity.this);
+                    dialog.setTitle("确认签到？");
+                    dialog.setCancelable(true);
+                    dialog.setPositiveButton("签到", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            if (selectedPerson.isSignIn()==true){
+                                Toast.makeText(ChoosePersonActivity.this, "已签到", Toast.LENGTH_SHORT).show();
+                            }else{
+                                selectedPerson.setSignIn(true);
+                                selectedPerson.save();
+                                Toast.makeText(ChoosePersonActivity.this, "签到成功", Toast.LENGTH_SHORT).show();
+                                Intent intent=new Intent(ChoosePersonActivity.this, MainActivity.class);
+                                startActivity(intent);
+                            }
+                        }
+                    });
+                    dialog.setNegativeButton("取消", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+
+                        }
+                    });
+                    dialog.show();
+                }
+            }
+        });
+
+        if(extra_nav==ALL_PERSON){
+            queryAllPerson();
+        }else if(extra_nav==WAIT_PERSON){
+            queryWaitPerson();
+        }else if(extra_nav==LEAVE_PERSON){
+            queryLeavePerson();
+        }else{
+            queryDepartment();
+        }
+
+    }
+
+    private void queryDepartment(){
+        toolbar.setTitle("所有部门");
+        departmantList= LitePal.findAll(Departmant.class);
+        if(departmantList.size()>0){
+            dataList.clear();
+            for (Departmant departmant:departmantList){
+                dataList.add(departmant.getName());
+            }
+            toolbar.setSubtitle(dataList.size()+"部门");
+            adapter.notifyDataSetChanged();
+            listView.setSelection(0);
+            currentLevel=LEVEL_DEPARTMENT;
+        }else{
+            Utility.createDepartmentData();
+            queryDepartment();
+        }
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onBackPressed();
+            }
+        });
+    }
+
+    private void queryPerson(){
+        toolbar.setTitle("部门人员");
+        personList=LitePal.where("Department=?",selectedDepartment.getName()).find(Person.class);
+        if(personList.size()>0){
+            dataList.clear();
+            for(Person person:personList){
+                if(person.isSignIn()==true){
+                    dataList.add(person.getName()+"——已签到");
+                }else{
+                    dataList.add(person.getName());
+                }
+
+            }
+            toolbar.setSubtitle(dataList.size()+"人");
+            adapter.notifyDataSetChanged();
+            listView.setSelection(0);
+            currentLevel=LEVEL_PERSON;
+        }else{
+            Utility.createPersonData();
+            queryPerson();
+        }
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(currentLevel==LEVEL_PERSON){
+                    queryDepartment();
+                }
+            }
+        });
+    }
+
+    private void queryAllPerson(){
+        toolbar.setTitle("所有人员");
+        personList=LitePal.findAll(Person.class);
+        if(personList.size()>0){
+            dataList.clear();
+            for(Person person:personList){
+                if(person.isSignIn()==true){
+                    dataList.add(person.getName()+"——已签到");
+                }else{
+                    dataList.add(person.getName());
+                }
+
+            }
+            toolbar.setSubtitle(dataList.size()+"人");
+            adapter.notifyDataSetChanged();
+            listView.setSelection(0);
+            currentLevel=ALL_PERSON;
+        }else{
+            Utility.createPersonData();
+            queryAllPerson();
+        }
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onBackPressed();
+            }
+        });
+    }
+
+    private void queryWaitPerson(){
+        toolbar.setTitle("暂未签到");
+        personList=LitePal.findAll(Person.class);
+        if(personList.size()>0){
+            dataList.clear();
+            for(Person person:personList){
+                if(person.isSignIn()==false){
+                    dataList.add(person.getName());
+                }
+            }
+            toolbar.setSubtitle(dataList.size()+"人");
+            adapter.notifyDataSetChanged();
+            listView.setSelection(0);
+            currentLevel=WAIT_PERSON;
+        }else{
+            Utility.createPersonData();
+            queryWaitPerson();
+        }
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onBackPressed();
+            }
+        });
+    }
+
+    private void queryLeavePerson(){
+        toolbar.setTitle("请假人员");
+        personList=LitePal.findAll(Person.class);
+        if(personList.size()>0){
+            dataList.clear();
+            for(Person person:personList){
+                if(person.isLeave()==true){
+                    dataList.add(person.getName());
+                }
+            }
+            toolbar.setSubtitle(dataList.size()+"人");
+            adapter.notifyDataSetChanged();
+            listView.setSelection(0);
+            currentLevel=LEAVE_PERSON;
+        }else{
+            Utility.createPersonData();
+            queryLeavePerson();
+        }
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onBackPressed();
+            }
+        });
     }
 }
--- app/src/main/java/com/liyinan/signin/activity/MainActivity.java
+++ app/src/main/java/com/liyinan/signin/activity/MainActivity.java
@@ -1,32 +1,56 @@
 package com.liyinan.signin.activity;
 
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.graphics.Color;
+import android.support.annotation.NonNull;
+import android.support.design.widget.CoordinatorLayout;
 import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.NavigationView;
+import android.support.design.widget.Snackbar;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.Gravity;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
 import com.liyinan.signin.R;
+import com.liyinan.signin.db.Departmant;
 import com.liyinan.signin.db.Person;
+import com.liyinan.signin.util.Utility;
 import com.liyinan.signin.view.AQIView;
 
 import org.litepal.LitePal;
 import org.w3c.dom.Text;
 
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+    public static final int ALL_PERSON=2;
+    public static final int WAIT_PERSON=3;
+    public static final int LEAVE_PERSON=4;
     private FloatingActionButton floatingActionButton;
     private AQIView aqiView;
     private Toolbar toolbar;
     private TextView signedNumText;
     private TextView leaveNumText;
     private List<Person> personList=new ArrayList<>();
-    Button button;
+    private List<Departmant> departmantList=new ArrayList<>();
+    private CoordinatorLayout coordinatorLayout;
+    private ImageView titleImage;
+    private NavigationView navigationView;
+    private DrawerLayout drawerLayout;
+    private TextView dateTextView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -36,26 +60,79 @@ public class MainActivity extends AppCompatActivity {
         toolbar=findViewById(R.id.main_tool_bar);
         signedNumText=findViewById(R.id.signed_num);
         leaveNumText=findViewById(R.id.leave_num);
+        coordinatorLayout=findViewById(R.id.coordinator_layout);
         //toolbar.setTitle("签到");
-        button=findViewById(R.id.button);
+        titleImage=findViewById(R.id.title_image);
+        navigationView=findViewById(R.id.main_nav_view);
+        drawerLayout=findViewById(R.id.main_drawer);
+        dateTextView=findViewById(R.id.main_date);
 
+        //读数据库，若为空则创建
+        personList=LitePal.findAll(Person.class);
+        if(personList.size()==0){
+            Utility.createPersonData();
+        }
+        departmantList=LitePal.findAll(Departmant.class);
+        if(departmantList.size()==0){
+            Utility.createDepartmentData();
+        }
+
+        //设置标题时间
+        Calendar calendar=Calendar.getInstance();
+        int month=calendar.get(Calendar.MONTH)+1;
+        int day=calendar.get(Calendar.DAY_OF_MONTH);
+        dateTextView.setText(month+"月"+day+"日");
+
+        //设置透明状态栏
+        View decorView=getWindow().getDecorView();
+        decorView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN|View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
+        getWindow().setStatusBarColor(Color.TRANSPARENT);
+
+        //设置菜单键
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                drawerLayout.openDrawer(GravityCompat.START);
+            }
+        });
+
+        //设置侧边栏
+        navigationView.setCheckedItem(R.id.nav_basic);
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                Intent intent=new Intent(MainActivity.this,ChoosePersonActivity.class);
+                switch (menuItem.getItemId()){
+                    case R.id.nav_all:
+                        intent.putExtra("extra_nav",ALL_PERSON);
+                        startActivity(intent);
+                        break;
+                    case R.id.nav_basic:
+                        drawerLayout.closeDrawers();
+                        break;
+                    case R.id.nav_leave:
+                        intent.putExtra("extra_nav",LEAVE_PERSON);
+                        startActivity(intent);
+                        break;
+                    case R.id.nav_wait:
+                        intent.putExtra("extra_nav",WAIT_PERSON);
+                        startActivity(intent);
+                        break;
+                    case R.id.nav_clear:
+                        drawerLayout.closeDrawers();
+                        clearAllData();
+                        break;
+                }
+                return true;
+            }
+        });
+
+        //设置悬浮按键
         floatingActionButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent=new Intent(MainActivity.this,ChoosePersonActivity.class);
                 startActivity(intent);
-            }
-        });
-
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                personList= LitePal.findAll(Person.class);
-                for (Person person:personList) {
-                    person.setSignIn(false);
-                    person.save();
-                }
-                setAqiView();
             }
         });
 
@@ -80,5 +157,45 @@ public class MainActivity extends AppCompatActivity {
         aqiView.setProgress(progress);
         signedNumText.setText(String.valueOf(num));
         leaveNumText.setText(String.valueOf(0));
+    }
+
+    //按两次退出
+    private long firstTime = 0;
+    @Override
+    public void onBackPressed() {
+        //第二次按返回键的时间戳
+        long secondTime = System.currentTimeMillis();
+        //如果第二次的时间戳减去第一次的时间戳大于2000毫秒，则提示再按一次退出，如果小于2000毫秒则直接退出。
+        if (secondTime - firstTime > 2000) {
+            //弹出是提示消息
+            Snackbar sb = Snackbar.make(coordinatorLayout, "再按一次退出", Snackbar.LENGTH_SHORT);
+            sb.show();
+            firstTime = secondTime;
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    private void clearAllData(){
+        AlertDialog.Builder dialog=new AlertDialog.Builder(MainActivity.this);
+        dialog.setTitle("确认清空数据？");
+        dialog.setPositiveButton("删除", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                personList= LitePal.findAll(Person.class);
+                for (Person person:personList) {
+                    person.setSignIn(false);
+                    person.save();
+                }
+                setAqiView();
+            }
+        });
+        dialog.setNegativeButton("取消", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+
+            }
+        });
+        dialog.show();
     }
 }
